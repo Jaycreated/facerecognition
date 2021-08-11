65f272488e4d1ff843ba2d63cf0b0e6451def1fc
Jay <johnsonjohnadakole@gmail.com>
2021-08-11 15:18:36 +0100

    my commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,40 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import { Component } from 'react';
 
-function App() {
+
+const ParticlesOptions = {
+          particles: {
+            number: {
+              value:30,
+              density: {
+               enable: true,
+               value_area: 800
+            }
+          }
+        }
+    }
+
+class App extends Component {
+render() {
   return (
     <div className="App">
+      <Particles className = 'particles'
+         params={ParticlesOptions}
+ 
+  />
         <Navigation/>
        <Logo/>
+       <Rank/>
         <ImageLinkForm/>
       {/*<FaceRecogniton/>*/}
 
     </div>
   );
 }
+};
 
 export default App;
